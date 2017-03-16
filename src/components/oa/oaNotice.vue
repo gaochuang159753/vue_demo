88d24a330a8bd128f17b0a328e7b75ea6c55055d
dahe <dahe@iyenei.com>
2017-03-16 20:06:36 +0800

    0316dahe

--- src/components/oa/oaNotice.vue
+++ src/components/oa/oaNotice.vue
@@ -1,14 +1,42 @@
 <template>
     <div class='oaNotice'>
-        工作台-公告
-        <el-button @click="ajax()">{{msg}}</el-button>
-        <el-button @click="alertBox()">{{msg}}</el-button>
-        <el-table :data="noticeList" style="width: 100%">
-            <el-table-column prop="createTime" label="日期" class="notice_td"></el-table-column>
-            <el-table-column prop="title" label="日期" class="notice_td"></el-table-column>
-            <el-table-column prop="content" label="日期" class="notice_td"></el-table-column>
-            <el-table-column prop="userName" label="日期" class="notice_td"></el-table-column>
-        </el-table>
+        <el-menu theme="light" :default-active="activeIndex" class="notice_nav" mode="horizontal">
+            <el-menu-item index="1" @click="activeIndex=1" >公告列表</el-menu-item>
+            <el-menu-item index="2" @click="activeIndex=2">新建公告</el-menu-item>
+        </el-menu>
+        <div class="notice_con" v-show="activeIndex==1">
+            <el-table :data="noticeList" style="width: 100%" align="center" class="notice_table">
+                <el-table-column prop="createTime" label="时间" class="notice_td"></el-table-column>
+                <el-table-column prop="title" label="标题" class="notice_td"></el-table-column>
+                <el-table-column prop="content" label="内容" class="notice_td"></el-table-column>
+                <el-table-column prop="userName" label="发送人" class="notice_td"></el-table-column>
+            </el-table>
+            <div class="notice_page">
+                <el-pagination layout="prev, pager, next" :total="noticeTotalPage" @current-change="changePage"></el-pagination>
+            </div>
+        </div>
+
+        <div class="notice_con" v-show="activeIndex==2">
+            <el-form label-position="right" label-width="150px" :model="noticeForm">
+                <el-form-item label="选择接收人" class="notice_form_input">
+                    <el-input v-model="noticeForm.name"></el-input>
+                </el-form-item>
+                <el-form-item label="公告标题" class="notice_form_input">
+                    <el-input v-model="noticeForm.title"></el-input>
+                </el-form-item>
+                <el-form-item label="公告签名" class="notice_form_input">
+                    <el-input v-model="noticeForm.type"></el-input>
+                </el-form-item>
+                <el-form-item label="公告内容" class="notice_form_input">
+                    <el-input v-model="noticeForm.type" type="textarea" row="4"></el-input>
+                </el-form-item>
+            </el-form>
+            <div>
+                <el-button @click="activeIndex=1">取消</el-button>
+                <el-button type="primary">发送</el-button>
+            </div>
+
+        </div>
     </div>
 </template>
 
@@ -18,24 +46,34 @@ export default {
     data () {
         return{
             noticeList: [],
-            msg: 'AJAX'
+            noticePage: 1,
+            noticeTotalPage: 0,
+            activeIndex: true,
+            noticeForm: '',
         }
-    },                                      
+    }, 
+    mounted: function (){
+        this.ajax(this.noticePage)
+    },                                 
    
     methods: {
-        ajax: function (){
+        console: function(){
+            console.log(this.activeIndex);
+        },
+        ajax: function (num){
             var self = this;
             this.$ajax.post('http://192.168.3.222:8080/hrm/api.do', 
                 'method=announcement/getAnnouncements'
                 +'&param='+JSON.stringify({
                     "userId": 61,
                     "companyId": 61,
-                    "pageNum": 1,
+                    "pageNum": num,
                     "pageSize": 10
                 })
             )
             .then(function (res) { 
                 self.noticeList = res.data.data.announcements;
+                self.noticeTotalPage = res.data.data.page.totalCount;
                 console.log(self.noticeList);
             })
             .catch(function (err) {
@@ -45,11 +83,23 @@ export default {
         alertBox: function(){
             this.noticeList[0] = 'helloworld';
             console.log(this.noticeList);
+        },
+        changePage: function(page){
+            this.noticePage = page
+            this.ajax(this.noticePage);
         }
     }
 }
 </script>
 
 <style scoped>
-
+.notice_nav{
+    margin: 30px 0;
+}
+.notice_page{
+    margin-top: 30px;
+}
+.notice_form_input{
+    width: 60%;
+}
 </style>
